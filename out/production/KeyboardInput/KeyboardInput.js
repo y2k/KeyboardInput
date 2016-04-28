5720495ed8d55d05544f08e2aa0e17ca73fd4eef
y2k <itwisterlx@gmail.com>
2016-04-28 19:37:25 +0400

    .

--- out/production/KeyboardInput/KeyboardInput.js
+++ out/production/KeyboardInput/KeyboardInput.js
@@ -2,6 +2,8 @@
   'use strict';
   var _ = Kotlin.defineRootPackage(null, /** @lends _ */ {
     main_kand9s$: function (args) {
+      var canvas = document.getElementsByTagName('CANVAS');
+      window.alert('ELEMENT = ' + canvas);
     }
   });
   Kotlin.defineModule('KeyboardInput', _);
