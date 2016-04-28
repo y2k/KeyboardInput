8ffecab9af151ee5fbd7deebb7fef762aa79ddee
y2k <itwisterlx@gmail.com>
2016-04-28 19:35:55 +0400

    .

---
+++ out/production/KeyboardInput/KeyboardInput.js
@@ -0,0 +1,9 @@
+(function (Kotlin) {
+  'use strict';
+  var _ = Kotlin.defineRootPackage(null, /** @lends _ */ {
+    main_kand9s$: function (args) {
+    }
+  });
+  Kotlin.defineModule('KeyboardInput', _);
+  _.main_kand9s$([]);
+}(Kotlin));
---
+++ src/App.kt
@@ -0,0 +1,14 @@
+import kotlin.browser.document
+import kotlin.browser.window
+
+/**
+ * Created by igor on 4/28/16.
+ */
+
+fun main(args: Array<String>) {
+    // TODO:
+
+    val canvas = document.getElementsByTagName("CANVAS")
+    window.alert("ELEMENT = " + canvas)
+
+}
